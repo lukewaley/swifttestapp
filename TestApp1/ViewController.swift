8b31f5c23ac3d3eb233149200e3f2d600b9ea955
Avacata <lwaley@avacata.com>
2017-07-31 15:13:14 -0500

    updated with boolean and ability to use product or sum

--- TestApp1/ViewController.swift
+++ TestApp1/ViewController.swift
@@ -20,34 +20,33 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: AnyObject) {
         
-        print(text1.text!)
-        print(text2.text!)
         theLabel.text = "success"
         //self.view.backgroundColor = UIColor.blue
         print("button tapped")
         tapCount = tapCount + 1
         print (tapCount)
-        if tapCount >= 5 {theLabel.text = "chill bro"
-            }
-          
-        theLabel.text = "product: \(Double (text1.text!)! * Double(text2.text!)!)"
+        if tapCount < 5 {theLabel.text = "product: \(Double (text1.text!)! * Double(text2.text!)!)"
+        } else {
+            
+            theLabel.text = "sum: \(Double (text1.text!)! + Double(text2.text!)!)"
+        }
         
     }
-   
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-    self.view.backgroundColor = UIColor.lightGray
+        
+        self.view.backgroundColor = UIColor.lightGray
         theLabel.text = "MyDinner"
-       
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
